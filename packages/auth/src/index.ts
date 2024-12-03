4565ba4dd8a2120a07139a9fae77c1da2d3f0904
biluadev <biluadevcod@gmail.com>
2024-12-03 16:13:19 +0100

    Permissões todas definidas

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -2,20 +2,32 @@ import {
     AbilityBuilder,
     CreateAbility, 
     createMongoAbility, 
-    ForcedSubject, 
     MongoAbility 
 } from '@casl/ability';
+import { z } from 'zod';
 import { User } from './models/user';
 import { permissions } from './permissions';
+import { userSubject } from './subjects/user';
+import { projectSubject } from './subjects/project';
+import { organizationSubject } from './subjects/organization';
+import { inviteSubject } from './subjects/invite';
+import { billingSubject } from './subjects/billing';
 
-const actions = ['manage', 'invite', 'delete'] as const;
-const subjects = ['User', 'all'] as const;
+export * from './models/organization';
+export * from './models/project';
+export * from './models/user';
 
-type AppAbilities = [
-  typeof actions[number],
-  typeof subjects[number] | ForcedSubject<Exclude<typeof subjects[number], 'all'>>
-];
+const appAbilitiesSchema = z.union([
+  projectSubject,
+  userSubject,
+  organizationSubject,
+  inviteSubject,
+  billingSubject,
 
+  z.tuple([z.literal('manage'), z.literal('all')])
+])
+
+type AppAbilities = z.infer<typeof appAbilitiesSchema>
 
 export type AppAbility = MongoAbility<AppAbilities>
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
@@ -29,7 +41,11 @@ export function defineAbilityFor(user: User) {
 
   permissions[user.role](user, builder)
 
-  const ability = builder.build()
+  const ability = builder.build({
+    detectSubjectType(subject) {
+      return subject.__typename
+    },
+  })
 
   return ability
 }
